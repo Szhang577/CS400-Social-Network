1e87cde210b3530e90fe7fa8cf1623163130c131
Yin <mac@Ruokais-MacBook-Pro.local>
2019-12-01 22:32:54 -0600

    upadte of advanceMenu gui

--- Social_Network/application/Main.java
+++ Social_Network/application/Main.java
@@ -34,233 +34,294 @@ import java.util.Random;
  */
 public class Main extends Application {
 
-	private static final double WINDOW_WIDTH = 1200;
-	private static final double WINDOW_HEIGHT = 800;
-	private static final String APP_TITLE = "Weibo Social Network";
+  private static final double WINDOW_WIDTH = 1200;
+  private static final double WINDOW_HEIGHT = 800;
+  private static final String APP_TITLE = "Weibo Social Network";
 
-	public SocialNetwork socialNetwork;
-	public Canvas graphCanvas;
-	private GraphicsContext gc;
-	public VBox statusGraphBox;
-	public HBox basicMenuBox;
-	public VBox advanceMenuBox;
-	private int numOfConnectedGraph;
-	private Random rand = new Random();
+  public SocialNetwork socialNetwork;
+  public Canvas graphCanvas;
+  private GraphicsContext gc;
+  public VBox statusGraphBox;
+  public HBox basicMenuBox;
+  public VBox advanceMenuBox;
+  private int numOfConnectedGraph;
+  private Random rand = new Random();
 
-	private void setUpgraphBox() {
+  private void setUpgraphBox() {
 
-	}
+  }
 
-	private void setUpstatusBox() {
+  private void setUpstatusBox() {
 
-	}
+  }
 
-	private void setUpBasicMenuBox() {
-		basicMenuBox = new HBox(10);
+  private void setUpBasicMenuBox() {
+    basicMenuBox = new HBox(10);
 
-		VBox buttonCollections = new VBox(20);
-		Button addUser = new Button("Add User");
-		Button removeUser = new Button("Remove User");
-		Button addFriends = new Button("Add Friendship");
-		Button removeFriends = new Button("Remove Friendship");
-		Button loadFile = new Button("Load File");
+    VBox buttonCollections = new VBox(20);
+    Button addUser = new Button("Add User");
+    Button removeUser = new Button("Remove User");
+    Button addFriends = new Button("Add Friendship");
+    Button removeFriends = new Button("Remove Friendship");
+    Button loadFile = new Button("Load File");
 
-		buttonCollections.getChildren().addAll(addUser, removeUser, addFriends, removeFriends, loadFile);
+    buttonCollections.getChildren().addAll(addUser, removeUser, addFriends, removeFriends,
+        loadFile);
 
-		VBox inputCollections = new VBox(20);
-		TextField addPerson = new TextField();
-		addPerson.setMaxWidth(410);
+    VBox inputCollections = new VBox(20);
+    TextField addPerson = new TextField();
+    addPerson.setMaxWidth(410);
 
-		TextField removePerson = new TextField();
-		removePerson.setMaxWidth(410);
+    TextField removePerson = new TextField();
+    removePerson.setMaxWidth(410);
 
-		HBox addFriendship = new HBox(10);
-		TextField addFriend1 = new TextField();
-		addFriend1.setMaxWidth(200);
-		TextField addFriend2 = new TextField();
-		addFriend2.setMaxWidth(200);
-		addFriendship.getChildren().addAll(addFriend1, addFriend2);
+    HBox addFriendship = new HBox(10);
+    TextField addFriend1 = new TextField();
+    addFriend1.setMaxWidth(200);
+    TextField addFriend2 = new TextField();
+    addFriend2.setMaxWidth(200);
+    addFriendship.getChildren().addAll(addFriend1, addFriend2);
 
-		HBox removeFriendship = new HBox(10);
-		TextField removeFriend1 = new TextField();
-		removeFriend1.setMaxWidth(200);
-		TextField removeFriend2 = new TextField();
-		removeFriend2.setMaxWidth(200);
-		removeFriendship.getChildren().addAll(removeFriend1, removeFriend2);
+    HBox removeFriendship = new HBox(10);
+    TextField removeFriend1 = new TextField();
+    removeFriend1.setMaxWidth(200);
+    TextField removeFriend2 = new TextField();
+    removeFriend2.setMaxWidth(200);
+    removeFriendship.getChildren().addAll(removeFriend1, removeFriend2);
 
-		TextField loadFileName = new TextField();
-		loadFileName.setMaxWidth(410);
+    TextField loadFileName = new TextField();
+    loadFileName.setMaxWidth(410);
 
-		inputCollections.getChildren().addAll(addPerson, removePerson, addFriendship, removeFriendship, loadFileName);
+    inputCollections.getChildren().addAll(addPerson, removePerson, addFriendship, removeFriendship,
+        loadFileName);
 
-		basicMenuBox.getChildren().addAll(buttonCollections, inputCollections);
+    basicMenuBox.getChildren().addAll(buttonCollections, inputCollections);
 
-		addUser.setOnAction((event) -> {
-			String name = addPerson.getText();
-			if (name != null && !name.isEmpty()) {
-				gc = graphCanvas.getGraphicsContext2D();
-				double x = (double) (rand.nextInt(600) + 50);
-				double y = (double) (rand.nextInt(600) + 50);
-				while (getNameFromCoordinates(x, y) != null) {
-					x = (double) (rand.nextInt(600) + 50);
-					y = (double) (rand.nextInt(600) + 50);
-				}
-				drawNode(gc, name, x, y);
-			} else {
-				System.out.println("name is null");
-			}
-		});
+    addUser.setOnAction((event) -> {
+      String name = addPerson.getText();
+      if (name != null && !name.isEmpty()) {
+        gc = graphCanvas.getGraphicsContext2D();
+        double x = (double) (rand.nextInt(600) + 50);
+        double y = (double) (rand.nextInt(600) + 50);
+        while (getNameFromCoordinates(x, y) != null) {
+          x = (double) (rand.nextInt(600) + 50);
+          y = (double) (rand.nextInt(600) + 50);
+        }
+        drawNode(gc, name, x, y);
+      } else {
+        System.out.println("name is null");
+      }
+    });
 
-		removeUser.setOnAction((event) -> {
-			String name = removePerson.getText();
-			if (name != null && !name.isEmpty()) {
-				gc = graphCanvas.getGraphicsContext2D();
-				double[] coord = getCoordinatesFromName(name);
-				removeNode(gc, coord[0], coord[1]);
-			} else {
-				System.out.println("name is null");
-			}
-		});
+    removeUser.setOnAction((event) -> {
+      String name = removePerson.getText();
+      if (name != null && !name.isEmpty()) {
+        gc = graphCanvas.getGraphicsContext2D();
+        double[] coord = getCoordinatesFromName(name);
+        removeNode(gc, coord[0], coord[1]);
+      } else {
+        System.out.println("name is null");
+      }
+    });
 
-		addFriends.setOnAction((event) -> {
-			String user1 = addFriend1.getText();
-			String user2 = addFriend2.getText();
-			if (user1 != null && user2 != null && !user1.isEmpty() && !user2.isEmpty()) {
-				gc = graphCanvas.getGraphicsContext2D();
-				double[] coord1 = getCoordinatesFromName(user1);
-				double[] coord2 = getCoordinatesFromName(user2);
-				drawEdge(gc, coord1[0], coord1[1], coord2[0], coord2[1]);
-			} else {
-				System.out.println("name is null");
-			}
-		});
+    addFriends.setOnAction((event) -> {
+      String user1 = addFriend1.getText();
+      String user2 = addFriend2.getText();
+      if (user1 != null && user2 != null && !user1.isEmpty() && !user2.isEmpty()) {
+        gc = graphCanvas.getGraphicsContext2D();
+        double[] coord1 = getCoordinatesFromName(user1);
+        double[] coord2 = getCoordinatesFromName(user2);
+        drawEdge(gc, coord1[0], coord1[1], coord2[0], coord2[1]);
+      } else {
+        System.out.println("name is null");
+      }
+    });
 
-		removeFriends.setOnAction((event) -> {
-			String user1 = removeFriend1.getText();
-			String user2 = removeFriend2.getText();
-			if (user1 != null && user2 != null && !user1.isEmpty() && !user2.isEmpty()) {
-				gc = graphCanvas.getGraphicsContext2D();
-				double[] coord1 = getCoordinatesFromName(user1);
-				double[] coord2 = getCoordinatesFromName(user2);
-				removeEdge(gc, coord1[0], coord1[1], coord2[0], coord2[1]);
-			} else {
-				System.out.println("name is null");
-			}
-		});
+    removeFriends.setOnAction((event) -> {
+      String user1 = removeFriend1.getText();
+      String user2 = removeFriend2.getText();
+      if (user1 != null && user2 != null && !user1.isEmpty() && !user2.isEmpty()) {
+        gc = graphCanvas.getGraphicsContext2D();
+        double[] coord1 = getCoordinatesFromName(user1);
+        double[] coord2 = getCoordinatesFromName(user2);
+        removeEdge(gc, coord1[0], coord1[1], coord2[0], coord2[1]);
+      } else {
+        System.out.println("name is null");
+      }
+    });
 
-		loadFile.setOnAction((event) -> {
-			String fileName = loadFileName.getText();
-			if (fileName != null && !fileName.isEmpty()) {
-				File file = new File(fileName);
-				socialNetwork.loadFromFile(file);
-			} else {
-				System.out.println("name is null");
-			}
-		});
-	}
+    loadFile.setOnAction((event) -> {
+      String fileName = loadFileName.getText();
+      if (fileName != null && !fileName.isEmpty()) {
+        File file = new File(fileName);
+        socialNetwork.loadFromFile(file);
+      } else {
+        System.out.println("name is null");
+      }
+    });
+  }
 
-	private void setUpAdvanceMenuBox() {
+  /**
+   * @author Ruokai
+   *
+   */
+  private void setUpAdvanceMenuBox() {
+    advanceMenuBox = new VBox(20);
 
-	}
+    HBox buttonCollection1 = new HBox(10);
+    Button clear = new Button("Clear");
+    Button undo = new Button("Undo");
+    Button exit = new Button("Exit");
 
-	private Label updateNumOfConnectedGroups(int numOfGroups) {
-		String numUpdate = "Number of Connected Groups is " + numOfGroups;
-		Label numLabel = new Label(numUpdate);
-		return numLabel;
+    HBox buttonCollection2 = new HBox(10);
+    Button export = new Button("Export");
+    TextField exportInp = new TextField();
+    exportInp.setMaxWidth(410);
 
-	}
+    HBox buttonCollection3 = new HBox(10);
+    Button mutual = new Button("Mutual Friend");
+    TextField mutualFriend1 = new TextField();
+    mutualFriend1.setMaxWidth(200);
+    TextField mutualFriend2 = new TextField();
+    mutualFriend2.setMaxWidth(200);
 
-	private void drawGraph(GraphicsContext graph) {
-		statusGraphBox = new VBox(20);
-		Label updateNum = updateNumOfConnectedGroups(this.numOfConnectedGraph);
-		gc.setFill(Color.BLUE);
-		gc.setFont(new Font(30));
-		gc.fillText("Weibo Social Network", 200, 600);
-		// Draw a line
-		// Lines use the stroke color
-		gc.setStroke(Color.BLUE);
-		gc.setLineWidth(2);
-		gc.strokeLine(40, 100, 250, 50);
-		gc.drawImage(createdTextedCircle("Mike"), 40 - 15, 100 - 15);
-		gc.drawImage(createdTextedCircle("Abby"), 250 - 15, 50 - 15);
-		gc.drawImage(createdTextedCircle("Eric"), 70, 200);
-		gc.drawImage(createdTextedCircle("Kevin"), 200, 300);
-		gc.drawImage(createdTextedCircle("Tim"), 230, 260);
-		gc.drawImage(createdTextedCircle("Jordon"), 180, 400);
-		statusGraphBox.getChildren().addAll(updateNum, graphCanvas);
+    HBox buttonCollection4 = new HBox(10);
+    Button sequence_friends = new Button("Sequence of Friends");
+    TextField sequence1 = new TextField();
+    sequence1.setMaxWidth(200);
+    TextField sequence2 = new TextField();
+    sequence2.setMaxWidth(200);
 
-	}
+    HBox buttonCollection5 = new HBox(10);
+    Button search = new Button("Search");
+    TextField searchInp = new TextField();
+    searchInp.setMaxWidth(410);
 
-	private WritableImage createdTextedCircle(String name) {
-		StackPane stackPane = new StackPane();
-		stackPane.setPrefSize(40, 40);
+    buttonCollection1.getChildren().addAll(clear, undo, exit);
+    buttonCollection2.getChildren().addAll(export, exportInp);
+    buttonCollection3.getChildren().addAll(mutual, mutualFriend1, mutualFriend2);
+    buttonCollection4.getChildren().addAll(sequence_friends,sequence1,sequence2);
+    buttonCollection5.getChildren().addAll(search, searchInp);
 
-		Circle circle = new Circle(40 / 2.0);
-		circle.setStroke(Color.YELLOW);
-		circle.setFill(Color.WHITE);
-		circle.setStrokeWidth(2);
-		stackPane.getChildren().add(circle);
 
-		Text textContent = new Text(name + "");
-		stackPane.getChildren().add(textContent);
-		SnapshotParameters parameters = new SnapshotParameters();
-		parameters.setFill(Color.TRANSPARENT);
-		return stackPane.snapshot(parameters, null);
-	}
+    advanceMenuBox.getChildren().addAll(buttonCollection1, buttonCollection2, buttonCollection3,
+        buttonCollection4, buttonCollection5);
+    
+    // actions
+    
+    search.setOnAction((click) -> {
+      String user = searchInp.getText();
+      if(user != null) {
+        setSelectedUser(user);
+      }else {
+        System.out.println("name is null");
+      }
+    });
+  }
 
-	private void drawNode(GraphicsContext graph, String name, double x, double y) {
+  private Label updateNumOfConnectedGroups(int numOfGroups) {
+    String numUpdate = "Number of Connected Groups is " + numOfGroups;
+    Label numLabel = new Label(numUpdate);
+    return numLabel;
 
-	}
+  }
 
-	private void removeNode(GraphicsContext graph, double x, double y) {
+  private void drawGraph(GraphicsContext graph) {
+    statusGraphBox = new VBox(20);
+    Label updateNum = updateNumOfConnectedGroups(this.numOfConnectedGraph);
+    gc.setFill(Color.BLUE);
+    gc.setFont(new Font(30));
+    gc.fillText("Weibo Social Network", 200, 600);
+    // Draw a line
+    // Lines use the stroke color
+    gc.setStroke(Color.BLUE);
+    gc.setLineWidth(2);
+    gc.strokeLine(40, 100, 250, 50);
+    gc.drawImage(createdTextedCircle("Mike"), 40 - 15, 100 - 15);
+    gc.drawImage(createdTextedCircle("Abby"), 250 - 15, 50 - 15);
+    gc.drawImage(createdTextedCircle("Eric"), 70, 200);
+    gc.drawImage(createdTextedCircle("Kevin"), 200, 300);
+    gc.drawImage(createdTextedCircle("Tim"), 230, 260);
+    gc.drawImage(createdTextedCircle("Jordon"), 180, 400);
+    statusGraphBox.getChildren().addAll(updateNum, graphCanvas);
 
-	}
+  }
 
-	private void drawEdge(GraphicsContext graph, double x1, double y1, double x2, double y2) {
+  private WritableImage createdTextedCircle(String name) {
+    StackPane stackPane = new StackPane();
+    stackPane.setPrefSize(40, 40);
 
-	}
+    Circle circle = new Circle(40 / 2.0);
+    circle.setStroke(Color.YELLOW);
+    circle.setFill(Color.WHITE);
+    circle.setStrokeWidth(2);
+    stackPane.getChildren().add(circle);
 
-	private void removeEdge(GraphicsContext graph, double x1, double y1, double x2, double y2) {
+    Text textContent = new Text(name + "");
+    stackPane.getChildren().add(textContent);
+    SnapshotParameters parameters = new SnapshotParameters();
+    parameters.setFill(Color.TRANSPARENT);
+    return stackPane.snapshot(parameters, null);
+  }
 
-	}
+  private void drawNode(GraphicsContext graph, String name, double x, double y) {
 
-	private String getNameFromCoordinates(double x1, double x2) {
-		return null;
+  }
 
-	}
+  private void removeNode(GraphicsContext graph, double x, double y) {
 
-	private double[] getCoordinatesFromName(String name) {
-		return null;
-	}
+  }
 
-	private void setSelectedUser(String person) {
+  private void drawEdge(GraphicsContext graph, double x1, double y1, double x2, double y2) {
 
-	}
+  }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		BorderPane pane = new BorderPane();
-		graphCanvas = new Canvas(750, 700);
-		gc = graphCanvas.getGraphicsContext2D();
-		drawGraph(gc);
-		setUpBasicMenuBox();
-		pane.setRight(basicMenuBox);
-		pane.setLeft(statusGraphBox);
-		Scene scene = new Scene(pane, WINDOW_WIDTH, WINDOW_HEIGHT);
+  private void removeEdge(GraphicsContext graph, double x1, double y1, double x2, double y2) {
 
-		primaryStage.setTitle(APP_TITLE);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+  }
 
-	}
+  private String getNameFromCoordinates(double x1, double x2) {
+    return null;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		launch(args);
-	}
+  }
+
+  private double[] getCoordinatesFromName(String name) {
+    return null;
+  }
+
+  private void setSelectedUser(String person) {
+
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    // TODO Auto-generated method stub
+    BorderPane pane = new BorderPane();
+    graphCanvas = new Canvas(750, 700);
+    gc = graphCanvas.getGraphicsContext2D();
+    drawGraph(gc);
+
+    VBox rightBox = new VBox(30);
+    setUpBasicMenuBox();
+    setUpAdvanceMenuBox();
+    rightBox.getChildren().addAll(basicMenuBox, advanceMenuBox);
+
+    pane.setRight(rightBox);
+    pane.setLeft(statusGraphBox);
+    Scene scene = new Scene(pane, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    primaryStage.setTitle(APP_TITLE);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+
+  }
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    launch(args);
+  }
 
 }
