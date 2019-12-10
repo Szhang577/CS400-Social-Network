59924855d36f2ef0a6c964b1738dfc25688fa55b
Yin <mac@Ruokais-MacBook-Pro.local>
2019-12-09 20:38:21 -0600

    update socialnetwork

--- Social_Network/application/SocialNetworkADT.java
+++ Social_Network/application/SocialNetworkADT.java
@@ -6,20 +6,34 @@ package application;
 import java.io.File;
 import java.util.List;
 import java.util.Set;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * @author Siyuan
  *
  */
 public interface SocialNetworkADT {
-  public boolean addFriends(String person1, String person2) throws DuplicatePersonException, PersonNotFoundException, DuplicateEdgesException;
-  public boolean removeFriends(String person1, String person2) throws PersonNotFoundException, NoEdgeExistsException;
+  public boolean addFriends(String person1, String person2)
+      throws DuplicatePersonException, PersonNotFoundException, DuplicateEdgesException;
+
+  public boolean removeFriends(String person1, String person2)
+      throws PersonNotFoundException, NoEdgeExistsException;
+
   public boolean addUser(String person) throws DuplicatePersonException;
+
   public boolean removeUser(String person) throws PersonNotFoundException;
+
   public Set<Person> getFriends(String person);
+
   public Set<Person> getMutualFriends(String person1, String person2);
+
   public List<Person> getShortestPath(String person1, String person2);
+
   public Set<Graph> getConnectedComponents();
-  public void loadFromFile(File file);
-  public void saveToFile(File file);
+
+  public void loadFromFile(File file) throws FileNotFoundException, DuplicatePersonException,
+      PersonNotFoundException, DuplicateEdgesException, NoEdgeExistsException;
+
+  public void saveToFile(File file) throws IOException;
 }
