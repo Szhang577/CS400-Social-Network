70857f678e240956b6d65f91f68451b9ed1b7d46
Erzhen Zhang <erzhen@royal-16.cs.wisc.edu>
2019-12-09 21:13:55 -0600

    <Erzhen> addition

--- Social_Network/application/Person.java
+++ Social_Network/application/Person.java
@@ -9,14 +9,24 @@ package application;
  */
 public class Person {
   private String name;
+  private boolean visited;
   public Person() {
     name = "";
   }
   public Person(String name) {
     this.name = name;
+    this.visited = false;
   }
   
   public String getName() {
     return name;
+  }
+  
+  public boolean getVisited() {
+	  return this.visited;
+  }
+  
+  public void setVisited(boolean status) {
+	  this.visited = status;
   }
 }
