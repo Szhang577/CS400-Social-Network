d59efc9acc10f8e39f7112f3b9d06d05d1379eef
Yin <mac@Ruokais-MacBook-Pro.local>
2019-12-11 02:39:54 -0600

    add comment Person

--- Social_Network/application/Person.java
+++ Social_Network/application/Person.java
@@ -1,34 +1,68 @@
-/**
- * 
- */
+/////////////////////////////// File Header ///////////////////////////////////
+//
+// Assignment name: ateam Final Project - Social Network
+// Author(s) and email addresses:
+// Erzhen Zhang, ezhang25@wisc.edu
+// Ruokai Yin, ryin25@wisc.edu
+// Seanna Zhang, szhang577@wisc.edu
+// Kaiwen Shen, kshen26@wisc.edu
+// Due date: Dec 11th
+// Other source credits:
+// Known bugs: None
+//
+///////////////////////////////////////////////////////////////////////////////
 package application;
 
 /**
- * @author Siyuan
- *
+ * 
+ * Class of a single person, contain basic getter and setter methods
+ * 
  */
 public class Person {
+
+  // private fields
   private String name;
   private boolean visited;
+
+  /**
+   * default constructor
+   */
   public Person() {
     name = "";
   }
+
+  /**
+   * default constructor with name
+   */
   public Person(String name) {
     this.name = name;
     this.visited = false;
   }
-  
+
+  /**
+   * private helper to get name
+   * 
+   * @return name of the person
+   */
   public String getName() {
     return name;
   }
-  
+
+  /**
+   * private helper to get status of been visited
+   * 
+   * @return status of visiting
+   */
   public boolean getVisited() {
-	  return this.visited;
+    return this.visited;
   }
-  
+
+  /**
+   * private helper to set the status of a person
+   */
   public void setVisited(boolean status) {
-	  this.visited = status;
+    this.visited = status;
   }
-  
-  
+
+
 }
